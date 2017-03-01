a04f9d2c0861e826ec36153e8a5e6ec6b11368aa
ande7195 <ande7195@edu.easj.dk>
2017-03-01 09:51:20 +0100

    Game running

--- Factory_Game/src/Main.java
+++ Factory_Game/src/Main.java
@@ -14,7 +14,20 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        RockHauler rockHauler = new RockHauler(5, 0, 10, 1);
+        SpearThrower spearThrower= new SpearThrower(5, 5, 20, 5);
+        Archer archer = new Archer(10, 10, 25, 10);
+        CrossbowMan crossbowMan= new CrossbowMan(10, 20, 30, 20);
+        Musketeer musketeer= new Musketeer(50, 50, 50, 50);
+        
+            
+        while(rockHauler.getHits()>0&&spearThrower.getHits()>0){
+            rockHauler.setName(" James");
+            spearThrower.setName(" Dean");
+            rockHauler.Attack(spearThrower);
+            spearThrower.Attack(rockHauler);
+            
+        }
     }
     
 }
